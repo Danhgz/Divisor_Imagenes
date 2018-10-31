03f403c5bdcc8541a616eab05472906ad37ecd1d
Mauricio Delgado Conejita Mala <mauriciodelgado790@gmail.com>
2018-10-31 17:07:42 -0600

    MAIN

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class Main here.
+ *
+ * @author Mauricio Delgado
+ * @author Daniel Henao Gómez
+ * @author Allan Barrantas
+ * 
+ * @version Diay, ya ni sé.
+ */
+public class Main{
+    public static void main(String ars[]){
+        
+        
+        System.exit(0);
+    }
+}
